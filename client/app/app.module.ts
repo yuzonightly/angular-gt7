50d9b2f08d43ec5e1c0e7f066fbac7c41d4c6ab7
yuzonightly <yuzonakai@gmail.com>
2022-01-25 00:05:05 +0000

    progress

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CarService } from './services/car.service';
+import { CircuitService } from './services/circuit.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { HomeComponent } from './home/home.component';
